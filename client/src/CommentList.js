813f2f0ec238ea5acc60d0ca68c1f60d81fa65c8
Shivam Kumar <shivam123425@gmail.com>
2021-01-16 01:21:23 +0530

    Create and wireup CommentList component

---
+++ client/src/CommentList.js
@@ -0,0 +1,24 @@
+import React, { useState, useEffect } from "react";
+import axios from "axios";
+
+function CommentList({ postId }) {
+  const [comments, setComments] = useState([]);
+
+  useEffect(() => {
+    async function getComments() {
+      const { data } = await axios.get(
+        `http://localhost:4001/posts/${postId}/comments`
+      );
+      setComments(data);
+    }
+    getComments();
+  }, [postId]);
+
+  const renderedComments = comments.map((comment) => {
+    return <li key={comment.id}>{comment.content}</li>;
+  });
+
+  return <ul>{renderedComments}</ul>;
+}
+
+export default CommentList;
--- client/src/PostList.js
+++ client/src/PostList.js
@@ -1,6 +1,7 @@
 import React, { useEffect, useState } from "react";
 import axios from "axios";
 import CommentCreate from "./CommentCreate";
+import CommentList from "./CommentList";
 
 function PostList() {
   const [posts, setPosts] = useState({});
@@ -22,6 +23,7 @@ function PostList() {
       >
         <div className="card-body">
           <h3>{post.title}</h3>
+          <CommentList postId={post.id} />
           <CommentCreate postId={post.id} />
         </div>
       </div>
