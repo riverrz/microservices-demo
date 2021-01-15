2cc0fb43068d7bd650624005c55c3e4cc39ae78f
Shivam Kumar <shivam123425@gmail.com>
2021-01-16 00:39:28 +0530

    create PostCreate component, introduce cors in posts and comments services

--- client/src/App.js
+++ client/src/App.js
@@ -1,7 +1,13 @@
 import React from "react";
+import PostCreate from "./PostCreate";
 
 function App() {
-  return <div>Blog app</div>;
+  return (
+    <div className="container">
+      <h1>Create posts</h1>
+      <PostCreate />
+    </div>
+  );
 }
 
 export default App;
