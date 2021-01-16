398f76a6f8cb9ac2f81637c7d072d2bd6a862754
Shivam Kumar <shivam123425@gmail.com>
2021-01-16 14:28:56 +0530

    create simple event bus

---
+++ event-bus/index.js
@@ -0,0 +1,19 @@
+const express = require("express");
+const bodyParser = require("body-parser");
+const axios = require("axios");
+
+const app = express();
+
+app.use(bodyParser.json());
+
+app.post("/events", (req, res) => {
+  const event = req.body;
+  axios.post("http://localhost:4000", event);
+  axios.post("http://localhost:4001", event);
+  axios.post("http://localhost:4002", event);
+  res.send({ status: "OK" });
+});
+
+app.listen(4005, () => {
+  console.log("Listening at 4005");
+});
