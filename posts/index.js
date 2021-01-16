07421433c5cd3b915d89dc07380b840f95a8a337
Shivam Kumar <shivam123425@gmail.com>
2021-01-16 14:34:27 +0530

    Emit PostCreated event

--- posts/index.js
+++ posts/index.js
@@ -2,6 +2,7 @@ const express = require("express");
 const bodyParser = require("body-parser");
 const cors = require("cors");
 const { randomBytes } = require("crypto");
+const axios = require("axios");
 
 const app = express();
 app.use(bodyParser.json());
@@ -12,10 +13,16 @@ app.get("/posts", (req, res) => {
   res.send(posts);
 });
 
-app.post("/posts", (req, res) => {
+app.post("/posts", async (req, res) => {
   const id = randomBytes(4).toString("hex");
   const { title } = req.body;
   posts[id] = { title, id };
+
+  await axios.post("http://localhost:4005/events", {
+    type: "PostCreated",
+    data: posts[id],
+  });
+
   res.status(201).send(posts[id]);
 });
 
