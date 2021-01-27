54cbc46e86f0c5e184dd6b76f9fb713fc36a6289
Shivam Kumar <shivam123425@gmail.com>
2021-01-27 14:28:39 +0530

    update event bus endpoint to use cluster ip service

--- comments/index.js
+++ comments/index.js
@@ -23,7 +23,7 @@ app.post("/posts/:id/comments", async (req, res) => {
   comments.push(comment);
   commentsByPostId[id] = comments;
 
-  await axios.post("http://localhost:4005/events", {
+  await axios.post("http://event-bus-srv:4005/events", {
     type: "CommentCreated",
     data: { ...comment, postId: id },
   });
@@ -42,7 +42,7 @@ app.post("/events", async (req, res) => {
     });
     comment.status = status;
 
-    await axios.post("http://localhost:4005/events", {
+    await axios.post("http://event-bus-srv:4005/events", {
       type: "CommentUpdated",
       data: {
         ...comment,
--- query/index.js
+++ query/index.js
@@ -41,7 +41,7 @@ app.post("/events", (req, res) => {
 app.listen(4002, async () => {
   console.log("Listening on 4002");
 
-  const { data } = await axios.get("http://localhost:4005/events");
+  const { data } = await axios.get("http://event-bus-srv:4005/events");
 
   for (let event of data) {
     console.log("Processing event:", event.type);
